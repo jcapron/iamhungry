38e13e2de7c20876ab6ed9f37b9a351dd405c9e6
jcapron <elniafron62@gmail.com>
2013-02-26 15:47:15 -0500

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Iamhungry::Application.config.secret_token = '7254c3008cfea8ca01b806abe596f51094d35124da951d180cab7127a30afe67c74e2ced62f0a81185357440e07d4907cf12777b4db49884b6d11e6b538525ef'
