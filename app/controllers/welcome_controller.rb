50a22c58a4f92e0aaa8b37a91ee7b9814abea4b8
jcapron <elniafron62@gmail.com>
2013-02-26 17:31:22 -0500

    added random examples in the search bar

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -5,6 +5,20 @@ def index
 	Punchr.api_key = Figleaf::Settings.punchr["dev_key"]
 	@client = Punchr::Client.new
 
+	examples = [
+	"Nutella and cream",
+	"Noodle, basil and chicken",
+	"Beef and honey",
+	"Turkey, cream and pasta",
+	"Salmon and rice",
+	"Apple and caramel",
+	"Nutella cookies",
+	"Pork and tomatoes",
+	"Shrimps and cream",
+	"Pork and pasta",
+	]
+	@example = examples[rand(examples.length)]
+
 	if params[:search]
 		@response = @client.recipes(:q => params[:search], :count => 50)
 		
